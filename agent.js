c1c72272654b3dc9687be4034c4b31b6163153a1
amartya149 <amartya970914@gmail.com>
2026-04-15 01:17:04 +0530

    Create ReAct agent

--- agent.js
+++ agent.js
@@ -1,9 +1,37 @@
-import { ChatGroq } from "@langchain/groq";
 import "dotenv/config";
+import { ChatGroq } from "@langchain/groq";
+import { createAgent } from "langchain";
+import { TavilySearch } from "@langchain/tavily";
 
-console.log("Agent working")
+async function main() {
+    const model = new ChatGroq({
+        model: "openai/gpt-oss-120b",
+        temperature: 0
+    });
 
-const model = new ChatGroq({
-model: "llama-3.3-70b-versatile",
-temperature: 0
-});
+    const search = new TavilySearch({
+        maxResults: 3,
+        topic: "general",
+    });
+
+    const agent = createAgent({
+        model,
+        tools: [search],
+    });
+
+
+    const result = await agent.invoke(
+        {
+            messages: [
+                {
+                    role: 'user',
+                    content: "How is the weather in bangalore now?"
+                }
+            ],
+        }
+    );
+
+    console.log("Assistant: ", result.messages[result.messages.length - 1].content);
+}
+
+main()
